99a9645a6efc30a7f088e719fd2b4521cce84ba1
Aiden Scandella <git@sca.ndella.com>
2024-07-14 22:34:31 -0700

    Switch to cross, add ctrlc and rppal

--- justfile
+++ justfile
@@ -1,9 +1,9 @@
-PI_ARCH := "aarch64-unknown-linux-gnu"
+PI_ARCH := "aarch64-unknown-linux-gnu" 
 PI_IP := "10.2.0.25"
 PROJECT_NAME := "workout-tracker-pi"
 
 build:
-	cargo zigbuild --release --target {{PI_ARCH}}
+	cross build --release --target {{PI_ARCH}}
 
 copy: build
 	scp target/{{PI_ARCH}}/release/{{PROJECT_NAME}} aiden@{{PI_IP}}:/home/aiden/{{PROJECT_NAME}}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::sync::mpsc::channel;
+
+use ctrlc;
+use rppal::gpio::{Gpio, Trigger};
+
+const GPIO_BUTTON: u8 = 26;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let (tx, rx) = channel::<()>();
+    let gpio = Gpio::new()?;
+    let mut pin = gpio.get(GPIO_BUTTON)?.into_input();
+
+    pin.set_async_interrupt(Trigger::RisingEdge, move |level| {
+        println!("Button pressed: {}", level)
+    })
+    .expect("Could not set async interrupt on pin.");
+
+    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+        .expect("Error setting Ctrl-C handler");
+    rx.recv().expect("Could not receive from channel.");
+    println!("Got it! Exiting...");
+
+    Ok(())
 }
