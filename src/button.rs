87b98e1d57c5d3d164be5410c584c4cd1c5abb36
Aiden Scandella <git@sca.ndella.com>
2024-07-14 23:15:02 -0700

    Refactor into button.rs

---
+++ src/button.rs
@@ -0,0 +1,40 @@
+use std::{
+    sync::{mpsc::Sender, Mutex},
+    time::{Duration, SystemTime},
+};
+
+pub(crate) struct DebouncedButton {
+    tx: Sender<()>,
+    duration: Duration,
+    last_press: Mutex<Option<SystemTime>>,
+}
+
+impl DebouncedButton {
+    pub fn new(tx: Sender<()>, debounce_duration: Duration) -> DebouncedButton {
+        DebouncedButton {
+            tx,
+            duration: debounce_duration,
+            last_press: Mutex::new(None),
+        }
+    }
+
+    pub fn pressed(&mut self) {
+        let mut last_press = self.last_press.lock().expect("Unable to acquire mutex");
+        let should_fire = match *last_press {
+            Some(last_press) => match last_press.elapsed() {
+                Ok(elapsed) => elapsed > self.duration,
+                Err(_) => {
+                    eprintln!("Unable to check elapsed time for debounce");
+                    true
+                }
+            },
+            None => true,
+        };
+        if should_fire {
+            self.tx
+                .send(())
+                .expect("Unable to send to button-fired channel");
+            *last_press = Some(SystemTime::now());
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,38 @@
 use std::error::Error;
 use std::sync::mpsc::channel;
+use std::thread;
+use std::time::Duration;
 
 use ctrlc;
 use rppal::gpio::{Gpio, Trigger};
 
+mod button;
+use button::DebouncedButton;
+
 const GPIO_BUTTON: u8 = 26;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (tx, rx) = channel::<()>();
+    let (exit_tx, exit_rx) = channel();
+    let (button_tx, button_rx) = channel();
     let gpio = Gpio::new()?;
     let mut pin = gpio.get(GPIO_BUTTON)?.into_input();
 
-    pin.set_async_interrupt(Trigger::FallingEdge, move |_level| {
-        println!("Button pressed")
-    })
-    .expect("Could not set async interrupt on pin.");
+    let mut button = DebouncedButton::new(button_tx, Duration::from_millis(100));
 
-    ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
+    pin.set_async_interrupt(Trigger::FallingEdge, move |_| button.pressed())
+        .expect("Could not set async interrupt on pin.");
+
+    ctrlc::set_handler(move || exit_tx.send(()).expect("Could not send signal on channel."))
         .expect("Error setting Ctrl-C handler");
-    rx.recv().expect("Could not receive from channel.");
-    println!("Got it! Exiting...");
+
+    thread::spawn(move || loop {
+        if let Ok(_) = button_rx.recv() {
+            println!("Button pressed!");
+        }
+    });
+
+    exit_rx.recv().expect("Could not receive from channel.");
+    println!("Received control-c. Exiting...");
 
     Ok(())
 }
