e6c7ef0a34b3d6b35d9e455e050eea7848b68d46
Aiden Scandella <git@sca.ndella.com>
2024-07-16 15:39:56 -0700

    Clean up some error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crossbeam_channel::{bounded, select, unbounded};
+use crossbeam_channel::{bounded, select};
 use gpiocdev::line::EdgeDetection;
 use std::error::Error;
 use std::time::Duration;
@@ -14,26 +14,26 @@ const GPIO_BUTTON: u32 = 26;
 const GPIO_CHIP: &str = "/dev/gpiochip0";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let (exit_tx, exit_rx) = bounded(1);
     let (button_tx, button_rx) = bounded(1);
+
     let pin_req = gpiocdev::Request::builder()
         .on_chip(GPIO_CHIP)
         .with_consumer("workout tracker")
         .with_line(GPIO_BUTTON)
         .with_edge_detection(EdgeDetection::FallingEdge)
-        .request()
-        .expect("GPIO button request");
+        .request()?;
 
     let mut button = DebouncedButton::new(button_tx, Duration::from_millis(500));
 
     std::thread::spawn(move || {
-        for event in pin_req.edge_events() {
+        for _event in pin_req.edge_events() {
             button.pressed();
         }
     });
 
-    ctrlc::set_handler(move || exit_tx.send(()).expect("Could not send signal on channel"))
-        .expect("Error setting Ctrl-C handler");
+    let (exit_tx, exit_rx) = bounded(1);
+
+    ctrlc::set_handler(move || exit_tx.send(()).expect("Could not send signal on channel"))?;
 
     let mut display = Display::new(GPIO_CHIP);
     display.text("Hello, world", display.height() / 2, display.width() / 2);
@@ -47,8 +47,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                 display.wake_up();
                 display.clear();
                 display.text(format!("Presses: {}", presses).as_str(), display.height() / 2, display.width() / 2);
-                display.sleep().expect("Unable to sleep");
-
+                display.sleep()?;
             }
             recv(exit_rx) -> _ => {
                 println!("Received control-c. Exiting...");
