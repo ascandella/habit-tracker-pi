8f52b60cab7aa6eb58c67fb2e517d596ea75f7d3
Aiden Scandella <git@sca.ndella.com>
2024-07-21 17:18:14 -0700

    Implement streak detection logic

--- crates/db/src/access_layer.rs
+++ crates/db/src/access_layer.rs
@@ -8,13 +8,47 @@ pub enum DataAccessError {
     SqliteError(#[from] rusqlite::Error),
     #[error("parse date error")]
     ParseDateError(#[from] chrono::ParseError),
+    #[error("timestamp parse error")]
+    TimestampParseError(String),
 }
 
+pub enum StreakData {
+    NoData,
+    Streak(Streak),
+}
+
+impl From<Vec<chrono::DateTime<chrono::Utc>>> for StreakData {
+    fn from(times: Vec<chrono::DateTime<chrono::Utc>>) -> Self {
+        if times.is_empty() {
+            StreakData::NoData
+        } else {
+            StreakData::Streak(Streak::new(times))
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Streak {
-    start: chrono::DateTime<chrono::Utc>,
-    count: usize,
     times: Vec<chrono::DateTime<chrono::Utc>>,
-    end: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+impl Streak {
+    fn new(times: Vec<chrono::DateTime<chrono::Utc>>) -> Self {
+        assert!(!times.is_empty());
+        Self { times }
+    }
+
+    pub fn len(&self) -> usize {
+        self.times.len()
+    }
+
+    pub fn start(&self) -> Option<&chrono::DateTime<chrono::Utc>> {
+        self.times.last()
+    }
+
+    pub fn end(&self) -> Option<&chrono::DateTime<chrono::Utc>> {
+        self.times.first()
+    }
 }
 
 impl AccessLayer {
@@ -25,35 +59,55 @@ impl AccessLayer {
     pub fn record_event(&self) -> Result<(), DataAccessError> {
         let now = std::time::SystemTime::now();
         let now: chrono::DateTime<chrono::Utc> = now.into();
-        self.record_event_at(now)
+        self.record_event_at(&now)
     }
 
     pub(crate) fn record_event_at(
         &self,
-        time: chrono::DateTime<chrono::Utc>,
+        time: &chrono::DateTime<chrono::Utc>,
     ) -> Result<(), DataAccessError> {
         self.conn.execute(
             "INSERT INTO events (timestamp) VALUES (?1)",
-            [time.to_rfc3339_opts(chrono::SecondsFormat::Millis, false)],
+            [sqlite_datetime(&time)],
         )?;
         Ok(())
     }
 
-    pub fn current_streak(&self, timezone: impl chrono::TimeZone) -> Result<(), DataAccessError> {
-        let fetch_size: u32 = 10;
+    pub fn current_streak(
+        &self,
+        timezone: &impl chrono::TimeZone,
+    ) -> Result<StreakData, DataAccessError> {
+        let now = chrono::Utc::now() + chrono::Duration::seconds(1);
+        self.streak_from_time(timezone, &now)
+    }
+
+    fn streak_from_time(
+        &self,
+        timezone: &impl chrono::TimeZone,
+        end: &chrono::DateTime<chrono::Utc>,
+    ) -> Result<StreakData, DataAccessError> {
+        let fetch_size: u32 = 100;
         let mut streak_alive = true;
-        let mut streak_end: Option<chrono::DateTime<chrono::Utc>> = None;
+        let mut streak_end = end.clone();
+        let mut dates = vec![];
 
         while streak_alive {
             // Return the current streak, based on querying the events table
-            let mut stmt = self
-                .conn
-                .prepare("SELECT * timestamp FROM events ORDER BY timestamp DESC LIMIT ?1")?;
+            let mut stmt = self.conn.prepare(
+                r#"
+                    SELECT timestamp FROM events
+                    WHERE timestamp < ?1
+                    ORDER BY timestamp DESC LIMIT ?2
+                "#,
+            )?;
             let rows = stmt
-                .query_map([fetch_size], |row| {
-                    let timestamp: String = row.get(0)?;
-                    Ok(timestamp)
-                })?
+                .query_map(
+                    [sqlite_datetime(&streak_end), fetch_size.to_string()],
+                    |row| {
+                        let timestamp: String = row.get(0)?;
+                        Ok(timestamp)
+                    },
+                )?
                 .collect::<Result<Vec<_>, _>>()?
                 .into_iter()
                 .map(|timestamp| {
@@ -61,15 +115,55 @@ impl AccessLayer {
                     Ok::<_, chrono::ParseError>(chrono::DateTime::<chrono::Utc>::from(res))
                 })
                 .collect::<Result<Vec<_>, _>>()?;
+
+            if rows.is_empty() {
+                break;
+            }
+
+            for &timestamp in &rows {
+                let end_comparison = dates.first().unwrap_or(&streak_end);
+
+                if is_previous_or_same_day(timezone, &timestamp, end_comparison) {
+                    dates.push(timestamp);
+                } else {
+                    streak_alive = false;
+                    break;
+                }
+            }
+
+            dates.last().map(|date| streak_end = *date);
         }
-        Ok(())
+
+        Ok(StreakData::from(dates))
     }
+}
+
+fn is_previous_or_same_day(
+    timezone: &impl chrono::TimeZone,
+    first: &chrono::DateTime<chrono::Utc>,
+    second: &chrono::DateTime<chrono::Utc>,
+) -> bool {
+    let first = first.with_timezone(timezone).date_naive();
+    let second = second.with_timezone(timezone).date_naive();
+    first == second || ((first - second).abs() == chrono::TimeDelta::days(1))
+}
+
+fn sqlite_datetime(time: &chrono::DateTime<chrono::Utc>) -> String {
+    time.to_rfc3339_opts(chrono::SecondsFormat::Millis, true)
 }
 
 #[cfg(test)]
 mod tests {
+    use chrono::TimeZone;
+
     use super::*;
     use crate::migrations;
+
+    fn create_access() -> AccessLayer {
+        let mut conn = rusqlite::Connection::open_in_memory().expect("open in-memory");
+        migrations::migrate(&mut conn).expect("migrate");
+        AccessLayer::new(conn)
+    }
 
     #[test]
     fn test_access_layer() {
@@ -78,5 +172,110 @@ mod tests {
         let access_layer = AccessLayer::new(conn);
         let test_resp = access_layer.record_event();
         assert!(test_resp.is_ok());
+    }
+
+    #[test]
+    fn test_sqlite_datetime() {
+        let dt: chrono::DateTime<chrono::Utc> = chrono::Utc
+            .with_ymd_and_hms(2024, 7, 21, 15, 30, 0)
+            .unwrap();
+        let time_str = sqlite_datetime(&dt);
+        assert_eq!(time_str, "2024-07-21T15:30:00.000Z");
+    }
+
+    #[test]
+    fn test_streak_no_data() {
+        let db = create_access();
+        let streak = db
+            .current_streak(&chrono::Utc)
+            .expect("fetch current streak");
+        assert!(matches!(streak, StreakData::NoData));
+    }
+
+    #[test]
+    fn test_streak_one_day() {
+        let db = create_access();
+        db.record_event().expect("record event");
+        let streak = db
+            .current_streak(&chrono::Utc)
+            .expect("fetch current streak");
+
+        match streak {
+            StreakData::Streak(streak) => {
+                assert_eq!(streak.len(), 1);
+                assert_eq!(
+                    streak.start().unwrap().date_naive(),
+                    chrono::Utc::now().date_naive()
+                );
+                assert_eq!(
+                    streak.end().unwrap().date_naive(),
+                    chrono::Utc::now().date_naive()
+                );
+            }
+            _ => panic!("expected streak"),
+        }
+    }
+
+    #[test]
+    fn test_streak_two_days() {
+        let db = create_access();
+        let now = chrono::Utc::now();
+        let dates = vec![
+            now,
+            now - chrono::Duration::days(1),
+            // Gap here, streak ends
+            now - chrono::Duration::days(3),
+            now - chrono::Duration::days(4),
+        ];
+        for date in dates {
+            db.record_event_at(&date).expect("record event");
+        }
+
+        let streak = db
+            .current_streak(&chrono::Utc)
+            .expect("fetch current streak");
+
+        match streak {
+            StreakData::Streak(streak) => {
+                assert_eq!(streak.len(), 2);
+                assert_eq!(streak.end().unwrap().date_naive(), now.date_naive());
+                assert_eq!(
+                    streak.start().unwrap().date_naive(),
+                    (now - chrono::Duration::days(1)).date_naive(),
+                );
+            }
+            _ => panic!("expected streak"),
+        }
+    }
+
+    #[test]
+    fn test_is_same_day_timezone() {
+        let timezone = chrono_tz::US::Pacific;
+        let dt: chrono::DateTime<chrono::Utc> = chrono::Utc
+            .with_ymd_and_hms(2024, 7, 21, 23, 30, 0)
+            .unwrap();
+        let yesterday = dt - chrono::Duration::days(1);
+        assert!(is_previous_or_same_day(&timezone, &dt, &dt));
+        assert!(is_previous_or_same_day(&timezone, &dt, &yesterday));
+        assert!(is_previous_or_same_day(&timezone, &yesterday, &dt));
+
+        let beginning_of_previous_day_pacific = chrono::Utc
+            .with_ymd_and_hms(2024, 7, 20, 13, 30, 0)
+            .unwrap();
+
+        assert!(is_previous_or_same_day(
+            &timezone,
+            &beginning_of_previous_day_pacific,
+            &dt
+        ));
+
+        let beginning_of_previous_day_utc =
+            chrono::Utc.with_ymd_and_hms(2024, 7, 20, 3, 30, 0).unwrap();
+
+        assert!(!is_previous_or_same_day(
+            &timezone,
+            &beginning_of_previous_day_utc,
+            &dt
+        ));
     }
 }
