6ce2ffdc0572b5d5076eea30a83183368a8e49e9
Aiden Scandella <git@sca.ndella.com>
2024-08-29 17:52:54 -0700

    Update rotation for new case

--- cli/src/display.rs
+++ cli/src/display.rs
@@ -47,7 +47,7 @@ impl Display {
 
         let mut display = Display2in7::default();
         // TODO: Make a configuration option
-        display.set_rotation(DisplayRotation::Rotate270);
+        display.set_rotation(DisplayRotation::Rotate90);
         // TODO: Make a configuration option
         let foreground_color = Color::Black;
         let background_color = Color::White;
